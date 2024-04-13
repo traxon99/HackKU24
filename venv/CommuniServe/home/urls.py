d13fa77a125fcdf368a5a7ac7fb6ce46c62f1250
Cole DuBois <cole.d5678@gmail.com>
2024-04-13 16:48:11 -0500

    Linked organizations page

--- venv/CommuniServe/home/urls.py
+++ venv/CommuniServe/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('home/signup_page/', views.signup_page, name='signup_page'),
+    path('home/organizations/', views.organizations, name='organizations'),
+    path('home/organizations/signup_page/', views.signup_page, name='signup_page'),
 ]
