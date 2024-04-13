f2ddadc6bf34d62e8d8f14bfbba92597957f611c
Traxon99 <yanekjackson@gmail.com>
2024-04-13 12:54:10 -0500

    deleted everything, recommitted everything

--- venv/CommuniServe/CommuniServe/wsgi.py
+++ venv/CommuniServe/CommuniServe/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HackKU24 project.
+WSGI config for CommuniServe project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HackKU24.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CommuniServe.settings')
 
 application = get_wsgi_application()
--- venv/CommuniServe/manage.py
+++ venv/CommuniServe/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HackKU24.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CommuniServe.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,6 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-#main function
+
 if __name__ == '__main__':
     main()
