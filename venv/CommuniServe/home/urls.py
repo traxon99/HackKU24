4a957dfa5e0a32e4f8f330c197e1958dfcdf1149
Traxon99 <yanekjackson@gmail.com>
2024-04-13 16:55:23 -0500

    auto populate the task list

--- venv/CommuniServe/home/urls.py
+++ venv/CommuniServe/home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from tasks.views import task_list  # Import the task_list view from the other app
+
 
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('home/signup_page/', views.signup_page, name='signup_page'),
+    path('tasks/', task_list, name='task_list'),
 ]
--- venv/CommuniServe/home/views.py
+++ venv/CommuniServe/home/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
+from tasks.models import Task  # Import the Task model from the other app
+from django.shortcuts import render
 
 def home(request):
-  template = loader.get_template('home.html')
-  return HttpResponse(template.render())
+    # Retrieve tasks from the database
+    tasks = Task.objects.all()
 
+    # Pass tasks data to the template
+    return render(request, 'home.html', {'tasks': tasks})
 def signup_page(request):
   template = loader.get_template('signup_page.html')
   return HttpResponse(template.render())
+
